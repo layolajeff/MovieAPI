a363679f5147b41b79833eaf2a697f3e3b5199f1
layolajeff <layolaj@yahoo.com>
2025-05-29 14:53:08 -0700

    still learning

--- script.js
+++ script.js
@@ -27,29 +27,3 @@ async function searchMovie() {
     container.innerHTML = `<p>No results found.</p>`;
   }
 }
-
-async function showMovieDetails(imdbID) {
-  const apiKey = "d2d06fc3"; 
-  const url = `https://www.omdbapi.com/?i=${imdbID}&apikey=${apiKey}`;
-
-  const response = await fetch(url);
-  const movie = await response.json();
-
-  const modalBody = document.getElementById("modalBody");
-  modalBody.innerHTML = `
-    <h2>${movie.Title} (${movie.Year})</h2>
-    <img src="${movie.Poster !== "N/A" ? movie.Poster : 'https://via.placeholder.com/300x450?text=No+Image'}" style="width: 200px; float: left; margin-right: 20px;" />
-    <p><strong>Genre:</strong> ${movie.Genre}</p>
-    <p><strong>Director:</strong> ${movie.Director}</p>
-    <p><strong>Actors:</strong> ${movie.Actors}</p>
-    <p><strong>Plot:</strong> ${movie.Plot}</p>
-    <p><strong>IMDB Rating:</strong> ${movie.imdbRating}</p>
-    <div style="clear: both;"></div>
-  `;
-
-  document.getElementById("movieModal").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("movieModal").style.display = "none";
-}
